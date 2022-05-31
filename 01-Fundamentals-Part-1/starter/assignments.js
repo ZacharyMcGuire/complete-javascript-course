661d1f045da7f0c60146e3390210b5129220d9ed
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 09:21:48 +1000

    lecture 11 assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -6,3 +6,12 @@ let population = 25690000;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+// LECTURE 11: Data Types
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
