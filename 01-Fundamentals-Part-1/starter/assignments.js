225c3cd8e94b4f6950979de93711cd0a4b7a3a0d
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 09:05:45 +1000

    Lecture 10 assignment

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,8 @@
+// LECTURE 10: Values and Variables
+let country = "Australia";
+let continent = "Australia";
+let population = 25690000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
