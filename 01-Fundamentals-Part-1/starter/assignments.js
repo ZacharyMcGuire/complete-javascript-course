5e554946b482080b564d7ce8c6934e1a98210bde
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 09:44:17 +1000

    lecture 13 assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,17 +1,20 @@
 // LECTURE 10: Values and Variables
-let country = "Australia";
-let continent = "Australia";
+const country = "Australia";
+const continent = "Australia";
 let population = 25690000;
 
 console.log(country);
 console.log(continent);
 console.log(population);
 
-// LECTURE 11: Data Types
-let isIsland = true;
+// LECTURE 12: Data Types
+const isIsland = true;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+// LECTURE 13: let, const & var
+language = "English";
