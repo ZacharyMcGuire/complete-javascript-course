5136d6a88dff4a41c934a318e51188ada9d92aa0
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 09:20:13 +1000

    lecture 11

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,25 @@ console.log(40 + 8 + 23 - 10);
 
 let firstName = "Jonas";
 console.log(firstName);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 9001);
+console.log(typeof firstName);
+
+javascriptIsFun = "Yes";
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1994;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
