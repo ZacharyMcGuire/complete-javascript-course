df9370001f686c653fb4aff2d19f50246f4ec170
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 12:46:24 +1000

    coding challenge 1

---
+++ 01-Fundamentals-Part-1/starter/codingChallenge1.js
@@ -0,0 +1,33 @@
+let markWeight;
+let markHeight;
+let markBMI;
+let johnWeight;
+let johnHeight;
+let johnBMI;
+let markHigherBMI;
+
+// Test Dataset 1
+markWeight = 78;
+markHeight = 1.69;
+johnWeight = 92;
+johnHeight = 1.95;
+
+markBMI = markWeight / (markHeight * markHeight);
+johnBMI = johnWeight / (johnHeight * johnHeight);
+markHigherBMI = markBMI > johnBMI;
+
+// 27.309968138370508 24.194608809993426 true
+console.log(markBMI, johnBMI, markHigherBMI);
+
+// Test Dataset 2
+markWeight = 95;
+markHeight = 1.88;
+johnWeight = 85;
+johnHeight = 1.76;
+
+markBMI = markWeight / (markHeight * markHeight);
+johnBMI = johnWeight / (johnHeight * johnHeight);
+markHigherBMI = markBMI > johnBMI;
+
+// 26.87867813490267 27.44059917355372 false
+console.log(markBMI, johnBMI, markHigherBMI);
