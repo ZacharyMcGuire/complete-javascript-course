fac8849c8db84871ce938720691acedd799d6575
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 16:01:20 +1000

    lecture 26 assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -70,3 +70,24 @@ if (language === "English" && population < 50000000 && !isIsland) {
 } else {
     console.log("Sarah should not live here...");
 }
+
+// LECTURE 26: Switch statement
+switch (language.toLowerCase()) {
+    case "chinese" || "mandarin":
+        console.log("MOST number of native speakers!");
+        break;
+    case "spanish":
+        console.log("2nd place in number of native speakers");
+        break;
+    case "english":
+        console.log("3rd place");
+        break;
+    case "hindi":
+        console.log("4th place");
+        break;
+    case "arabic":
+        console.log("5th place");
+        break;
+    default:
+        console.log("Great language too! :D");
+}
