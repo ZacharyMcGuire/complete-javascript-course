8d1436a943f95273e457a357bac11f08482c46f9
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-01 14:48:02 +1000

    coding challenge 2

--- 01-Fundamentals-Part-1/starter/codingChallenge.js
+++ 01-Fundamentals-Part-1/starter/codingChallenge.js
@@ -19,6 +19,12 @@ markHigherBMI = markBMI > johnBMI;
 // 27.309968138370508 24.194608809993426 true
 console.log(markBMI, johnBMI, markHigherBMI);
 
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's! (${markBMI})`);
+}
+
 // Test Dataset 2
 markWeight = 95;
 markHeight = 1.88;
@@ -31,3 +37,9 @@ markHigherBMI = markBMI > johnBMI;
 
 // 26.87867813490267 27.44059917355372 false
 console.log(markBMI, johnBMI, markHigherBMI);
+
+if (markHigherBMI) {
+    console.log(`Mark's BMI (${markBMI}) is higher than John's! (${johnBMI})`);
+} else {
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's! (${markBMI})`);
+}
