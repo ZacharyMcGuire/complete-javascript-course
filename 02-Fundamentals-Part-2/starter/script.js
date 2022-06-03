4c938c1fdaac61936c4adc992c0ba9bdfcab83dc
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-03 14:06:17 +1000

    Coding Challenge 2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -98,3 +98,26 @@ if (!neighbours.includes("Germany")) console.log("Probably not a central Europea
 const i = neighbours.indexOf("Sweden");
 neighbours[1] = "Republic of Sweden";
 console.log(neighbours);
+
+// Coding Challenge #2
+const calcTip = function (bill) {
+    const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+    return tip;
+}
+
+console.log(calcTip(100));
+
+const bills = [125, 555, 44];
+const tips = [
+    calcTip(bills[0]),
+    calcTip(bills[1]),
+    calcTip(bills[2])
+]
+console.log(tips);
+
+const total = [
+    bills[0] + tips[0],
+    bills[1] + tips[1],
+    bills[2] + tips[2]
+]
+console.log(total);
