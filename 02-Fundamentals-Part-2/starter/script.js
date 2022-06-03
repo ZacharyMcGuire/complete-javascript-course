a94116a597017c58ca1ca7c8fd2c365386d13d7f
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-03 14:56:17 +1000

    dot vs bracket notation assignment.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -137,3 +137,8 @@ console.log(
     `${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`
 )
 
+myCountry.population += 2;
+console.log(myCountry);
+
+myCountry['population'] -= 2;
+console.log(myCountry);
