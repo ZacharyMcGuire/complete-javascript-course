1beef6b807ab491fe010ff1e0801f8efb49376ce
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-06-03 12:38:47 +1000

    function declaration and expression assignment.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -12,3 +12,23 @@ const canada = describeCountry('Canada', 37, 'Ottawa');
 const france = describeCountry('France', 66, 'Paris');
 
 console.log(australia, canada, france);
+
+// LECTURE: Function declarations vs. function expressions
+function percentageOfWorld1 (population) {
+    return (population / 7900) * 100;
+}
+
+let chinaPopulationPercentage = percentageOfWorld1(1441);
+let australiaPopulationPercentage = percentageOfWorld1(24);
+let newZealandPopulationPercentage = percentageOfWorld1(14);
+console.log(chinaPopulationPercentage, australiaPopulationPercentage, newZealandPopulationPercentage);
+
+const percentageOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+}
+
+chinaPopulationPercentage = percentageOfWorld2(1441);
+australiaPopulationPercentage = percentageOfWorld2(24);
+newZealandPopulationPercentage = percentageOfWorld2(14);
+console.log(chinaPopulationPercentage, australiaPopulationPercentage, newZealandPopulationPercentage);
+
