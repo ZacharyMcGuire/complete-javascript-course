94c3a83cf4f3f29848d444e822e6a0e59d5ea1a7
Zachary McGuire <zacharysjmcguire@gmail.com>
2022-08-05 16:19:32 +1000

    03 Coding Challenge 1.

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,15 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// Coding Challenge #1
+
+const printForecast = function (arr) {
+  let message = "";
+  for (let i = 0; i < arr.length; i++) {
+    message += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log(message);
+};
+
+printForecast([17, 21, 23]);
+printForecast([12, 5, -5, 0, 4]);
